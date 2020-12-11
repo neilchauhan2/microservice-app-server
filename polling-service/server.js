006d5010d4001c020e5aeb2458082672628d55f5
Neil Chauhan <neilchauhan2@gmail.com>
2020-12-11 19:58:50 +0530

    Docker and nginx added

--- polling-service/server.js
+++ polling-service/server.js
@@ -2,29 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(
-    process.env.MONGODB_URI,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 app.use("/api/polling", require("./routes/poll"));
 
 app.get("/", (req, res) => {
-    res.send("<h1> Hello World! </h1>");
+  res.send("<h1> Hello World! </h1>");
 });
 
 app.listen(8000, () => {
-    console.log("Server started at http://localhost:8000");
+  console.log("Server started at http://localhost:8000");
 });
-
