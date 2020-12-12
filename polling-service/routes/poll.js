ad5468e54d976c3487393c656ceb1532b00bb678
Neil Chauhan <neilchauhan2@gmail.com>
2020-12-12 15:08:09 +0530

    routes updated

--- polling-service/routes/poll.js
+++ polling-service/routes/poll.js
@@ -7,16 +7,6 @@ const {
 } = require("../controllers/nomination");
 const { createPoll, getPoll, getPolls } = require("../controllers/poll");
 
-const Pusher = require("pusher");
-
-const pusher = new Pusher({
-  appId: "1118715",
-  key: "1eb7f0ffa38cc3d46a7c",
-  secret: "decebf66d6f2c4acb55e",
-  cluster: "ap2",
-  useTLS: true,
-});
-
 // get poll
 route.get("/poll/:id", async (req, res) => {
   try {
@@ -76,10 +66,7 @@ route.post("/nomination/upvote/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const nomination = await upVote(id);
-    pusher.trigger("poll", "vote", {
-      ...nomination,
-    });
-    res.send("Thank you for voting.");
+    res.send(nomination);
   } catch (error) {
     console.log(error);
   }
@@ -90,12 +77,7 @@ route.post("/nomination/downvote/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const nomination = await downVote(id);
-    pusher.trigger("poll", "vote", {
-      title: nomination.title,
-      count: nomination.count,
-      id: nomination.id,
-    });
-    res.send("Your response has been recorded.");
+    res.send(nomination);
   } catch (error) {
     console.log(error);
   }
